bbe34f4d21bb5c35689b0436bd02c5ce6b5bcb06
Abhra Kar <abhra.kar@gmail.com>
2024-10-29 16:19:34 +0530

    Create L14_LongestCommonPrefix.java

---
+++ Top150/Array_String/L14_LongestCommonPrefix.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class L14_LongestCommonPrefix {
+	public static void main(String[] args) {
+		String[] strs = {"dog", "racecar", "car"};
+    //String[] strs = {"flower","flow","flight"};
+		String result = longestCommonPrefix(strs);
+		System.out.println("Longest Common Prefix == " + result);
+	}
+
+	public static String longestCommonPrefix(String[] strs) {
+		Arrays.sort(strs);
+		String first = strs[0];
+		String last = strs[strs.length - 1];
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < first.length(); i++) {
+			if (first.charAt(i) != last.charAt(i)) {
+				break;
+			}
+			sb.append(first.charAt(i));
+		}
+
+		return sb.toString();
+	}
+}
