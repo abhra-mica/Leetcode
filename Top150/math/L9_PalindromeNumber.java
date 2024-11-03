d1462df504999df050f42c5805d8b6fa402c4268
Abhra Kar <abhra.kar@gmail.com>
2024-11-03 17:03:52 +0530

    Create L9_PalindromeNumber.java

---
+++ Top150/math/L9_PalindromeNumber.java
@@ -0,0 +1,34 @@
+package math;
+
+public class L9_PalindromeNumber {
+	public static void main(String[] args) {
+		int x = 10;
+		boolean result = isPalindrome(x);
+		System.out.println("Palindrome = " + result);
+
+	}
+
+	public static boolean isPalindrome(int x) {
+		if (x < 0 || (x % 10 == 0 && x != 0)) {// Suppose 1210, ending with zero
+												// can't be a palindrome. x!=0
+												// ==> if the number is only
+												// zero then itself a palindrome
+												// number
+			return false;
+		}
+
+		int reversedNo = 0;
+		// Through below code we are checking half of the number.
+		while (x > reversedNo) {
+			reversedNo = reversedNo * 10 + x % 10;
+			x = x / 10;
+		}
+
+		return (x == reversedNo) || x == reversedNo / 10; // suppose 121, one
+															// point x=1 and
+															// reversedNo=12
+															// then the second
+															// or condition will
+															// work
+	}
+}
