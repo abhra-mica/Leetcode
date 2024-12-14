1db5228edb5b6ded9eef180f454ab0e10929f46b
Abhra Kar <abhra.kar@gmail.com>
2024-12-14 22:35:29 +0530

    Create L452_MinimumNumberOfArrowsToBurstBalloons.java

---
+++ Top150/Intervals/L452_MinimumNumberOfArrowsToBurstBalloons.java
@@ -0,0 +1,27 @@
+package intervals;
+
+import java.util.Arrays;
+
+public class L452_MinimumNumberOfArrowsToBurstBalloons {
+	public static void main(String[] args) {
+		int[][] points = {{10, 16}, {2, 8}, {1, 6}, {7, 12}};
+		int result = findMinArrowShots(points);
+		System.out.println("Balllons Required= " + result);
+	}
+
+	public static int findMinArrowShots(int[][] points) {
+		Arrays.sort(points, (a, b) -> Integer.compare(a[1], b[1]));
+		int balloons = 1;
+		int[] currPoint = points[0];
+
+		for (int i = 1; i < points.length; i++) {
+			if (currPoint[1] >= points[i][0]) {
+				continue;
+			} else {
+				balloons++;
+				currPoint = points[i];
+			}
+		}
+		return balloons;
+	}
+}
