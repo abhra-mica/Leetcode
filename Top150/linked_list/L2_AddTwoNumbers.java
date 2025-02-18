31010239a498427a2205a9da72566eb2c76fb821
Abhra Kar <abhra.kar@gmail.com>
2025-02-18 13:25:18 +0530

    Update L2_AddTwoNumbers.java

--- Top150/linked_list/L2_AddTwoNumbers.java
+++ Top150/linked_list/L2_AddTwoNumbers.java
@@ -13,32 +13,53 @@ public class L2_AddTwoNumbers {
 
 		ListNode resultHead = addTwoNumbers(list1, list2);
 		System.out.println(resultHead);
-
 	}
 
-	public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-		ListNode dummy = new ListNode(0);
-		ListNode current = dummy;
+	public static ListNode addTwoNumbers(ListNode node1, ListNode node2) {
+		ListNode dummy = new ListNode(-1);
+		ListNode result = dummy;
 		int carry = 0;
-
-		while (l1 != null || l2 != null || carry == 1) {
-			int sum = carry;
-
-			if (l1 != null) {
-				sum += l1.val;
-				l1 = l1.next;
-			}
-
-			if (l2 != null) {
-				sum += l2.val;
-				l2 = l2.next;
-			}
-
-			carry = sum / 10;
-			current.next = new ListNode(sum % 10);
-			current = current.next;
+		while (node1 != null && node2 != null) {
+			dummy.next = new ListNode((node1.val + node2.val + carry) % 10);
+			carry = (node1.val + node2.val + carry) / 10;
+			dummy = dummy.next;
+			node1 = node1.next;
+			node2 = node2.next;
 		}
-		return dummy.next;
+		// Any of the linkedlist may over
+		while (node1 != null) {
+			dummy.next = new ListNode((carry + node1.val) % 10);
+			carry = (node1.val + carry) / 10;
+			dummy = dummy.next;
+			node1 = node1.next;
+		}
+
+		while (node2 != null) {
+			dummy.next = new ListNode((carry + node2.val) % 10);
+			carry = (node2.val + carry) / 10;
+			dummy = dummy.next;
+			node2 = node2.next;
+		}
+		// last digits say (8 + 9) == 17 so carry 1 will be present which we
+		// need to add to the list
+		if (carry != 0) {
+			dummy.next = new ListNode(carry);
+		}
+
+		return result.next;
 	}
 
+	/*
+	 * public static ListNode addTwoNumbers(ListNode l1, ListNode l2) { ListNode
+	 * dummy = new ListNode(0); ListNode current = dummy; int carry = 0;
+	 * 
+	 * while (l1 != null || l2 != null || carry == 1) { int sum = carry;
+	 * 
+	 * if (l1 != null) { sum += l1.val; l1 = l1.next; }
+	 * 
+	 * if (l2 != null) { sum += l2.val; l2 = l2.next; }
+	 * 
+	 * carry = sum / 10; current.next = new ListNode(sum % 10); current =
+	 * current.next; } return dummy.next; }
+	 */
 }
