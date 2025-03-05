9b280f3142fcda97e20beda61321c02a945b6466
Abhra Kar <abhra.kar@gmail.com>
2025-03-05 09:18:52 +0530

    Update L222_CountCompleteTreeNodes.java

--- Top150/binary_tree_general/L222_CountCompleteTreeNodes.java
+++ Top150/binary_tree_general/L222_CountCompleteTreeNodes.java
@@ -4,9 +4,35 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class L222_CountCompleteTreeNodes {
+	private static int count = 0;
+	
 	public static void main(String[] args) {
+		TreeNode root = new TreeNode(1);
+		root.left = new TreeNode(2);
+		root.right = new TreeNode(3);
+
+		root.left.left = new TreeNode(4);
+		root.left.right = new TreeNode(5);
+
+		root.right.left = new TreeNode(6);
+
+		int result = countNodes(root);
+		System.out.println("Total Nodes= " + result);
 	}
 
+
+	//Recursive way
+	public static int countNodes(TreeNode root) {
+		if (root == null) {
+			return count;
+		}
+
+		countNodes(root.left);
+		countNodes(root.right);
+		return ++count;
+	}
+
+	//BFS Approach
 	public static int countNodes(TreeNode root) {
 		if (root == null) {
 			return 0;
