33de933d01e6abd0026b482db16a5e295fd801a1
Abhra Kar <abhra.kar@gmail.com>
2025-03-06 20:12:38 +0530

    Update L117_PopulatingNextRightPointersinEachNodeII.java

--- Top150/binary_tree_general/L117_PopulatingNextRightPointersinEachNodeII.java
+++ Top150/binary_tree_general/L117_PopulatingNextRightPointersinEachNodeII.java
@@ -8,6 +8,31 @@ public class L117_PopulatingNextRightPointersinEachNodeII {
 
 	}
 
+	public Node connect_optimized_space_complexity(Node root) {
+		Node head = root;
+		while (head != null) {
+			Node dummy = new Node(-1);
+			Node temp = dummy;
+
+			while (head != null) {
+				if (head.left != null) {
+					temp.next = head.left;
+					temp = temp.next;
+				}
+
+				if (head.right != null) {
+					temp.next = head.right;
+					temp = temp.next;
+				}
+
+				head = head.next;
+			}
+			head = dummy.next;
+		}
+		return root;
+	}
+
+	
 	public static Node connect(Node root) {
 		if (root == null) {
 			return root;
