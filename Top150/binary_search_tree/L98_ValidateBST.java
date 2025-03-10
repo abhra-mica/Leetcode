208fc59231541371d57b71947a233f8ada1a26a7
Abhra Kar <abhra.kar@gmail.com>
2025-03-10 19:05:37 +0530

    Update L98_ValidateBST.java

--- Top150/binary_search_tree/L98_ValidateBST.java
+++ Top150/binary_search_tree/L98_ValidateBST.java
@@ -1,34 +1,37 @@
 package binary_search_tree;
 
-import java.util.ArrayList;
-import java.util.List;
+import binary_tree_general.TreeNode;
 
 public class L98_ValidateBST {
-	private static List<Integer> list = new ArrayList<>();
+	static boolean isValid = true;
+	static Integer prev = null;
+
 	public static void main(String[] args) {
-		TreeNode root = new TreeNode(0);
+		TreeNode root = new TreeNode(4);
+		root.left = new TreeNode(2);
+		root.right = new TreeNode(6);
+
 		boolean result = isValidBST(root);
-		System.out.println("Valid Binary Search Tree= " + result);
+		System.out.println("Result= " + result);
+
 	}
 
-	// Code is working fine in eclipse but in leetcode it is failing for [0]
-	// node saying return false.
 	public static boolean isValidBST(TreeNode root) {
-		inorder(root);
-		for (int i = 0; i < list.size() - 1; i++) {
-			if (list.get(i) > list.get(i + 1)) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-	private static void inorder(TreeNode root) {
 		if (root == null) {
-			return;
+			return isValid;
 		}
-		inorder(root.left);
-		list.add(root.val);
-		inorder(root.right);
+
+		isValidBST(root.left);
+
+		if (prev != null && isValid) {
+			isValid = prev < root.val ? true : false;
+		}
+
+		if (root != null) {
+			prev = root.val;
+		}
+
+		isValidBST(root.right);
+		return isValid;
 	}
 }
