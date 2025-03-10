7c5a04bc97aeed7cdb7e76a5831d71f5f5f7d35f
Abhra Kar <abhra.kar@gmail.com>
2025-03-10 16:54:00 +0530

    Update L230_KthSmallestElementBST.java

--- Top150/binary_search_tree/L230_KthSmallestElementBST.java
+++ Top150/binary_search_tree/L230_KthSmallestElementBST.java
@@ -1,10 +1,9 @@
-package binary_search_tree;
+package TEST;
+import binary_tree_general.TreeNode;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class L230_KthSmallestElementBST {
-	private static List<Integer> list = new ArrayList<>();
+public class Test {
+	static int count = 0;
+	static int kthSmallest = 0;
 
 	public static void main(String[] args) {
 		TreeNode root = new TreeNode(4);
@@ -12,21 +11,22 @@ public class L230_KthSmallestElementBST {
 		root.right = new TreeNode(5);
 		root.left.right = new TreeNode(3);
 
-		int result = kthSmallest(root, 1);
-		System.out.println(1 + "th smallest = " + result);
+		int result = kthSmallest(root, 2);
+		System.out.println(result);
 	}
 
 	public static int kthSmallest(TreeNode root, int k) {
-		inorder(root);
-		return list.get(k - 1);
-	}
-
-	private static void inorder(TreeNode root) {
 		if (root == null) {
-			return;
+			return count;
 		}
-		inorder(root.left);
-		list.add(root.val);
-		inorder(root.right);
+
+		kthSmallest(root.left, k);
+		count++;
+		if (count == k) {
+			kthSmallest = root.val;
+		}
+
+		kthSmallest(root.right, k);
+		return kthSmallest;
 	}
 }
