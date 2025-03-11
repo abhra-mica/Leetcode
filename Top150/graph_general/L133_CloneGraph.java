f5bf6414aa14db8c7475929860ef54bbdc475a9d
Abhra Kar <abhra.kar@gmail.com>
2025-03-11 16:33:59 +0530

    Update L133_CloneGraph.java

--- Top150/graph_general/L133_CloneGraph.java
+++ Top150/graph_general/L133_CloneGraph.java
@@ -8,34 +8,56 @@ import java.util.Map;
 import java.util.Queue;
 
 public class L133_CloneGraph {
+	Map<Node, Node> cloneMap = new HashMap<>();
 	public static void main(String[] args) {
 
 	}
 
-	public static Node cloneGraph(Node node) {
+	public Node cloneGraphDFS(Node node) {
 		if (node == null) {
 			return null;
 		}
 
-		Map<Node, Node> map = new HashMap<>();
-		Queue<Node> queue = new LinkedList<>();
+		Node newNode = new Node(node.val);
+		cloneMap.put(node, newNode); // Node is not present, checked through neighour check
 
-		queue.add(node);
-		map.put(node, new Node(node.val, new ArrayList<Node>()));
-
-		while (!queue.isEmpty()) {
-			Node h = queue.poll();
-
-			for (Node neighbour : h.neighbors) {
-				if (!map.containsKey(neighbour)) {
-					map.put(neighbour,
-							new Node(neighbour.val, new ArrayList<Node>()));
-					queue.add(neighbour);
-				}
-				map.get(h).neighbors.add(map.get(neighbour));
+		for (Node neighbour : node.neighbors) {
+			if (cloneMap.get(neighbour) == null) {
+				newNode.neighbors.add(cloneGraphDFS(neighbour));
+			} else {
+				newNode.neighbors.add(cloneMap.get(neighbour)); // This is the recursion break condition
 			}
 		}
-		return map.get(node);
+
+		return cloneMap.get(node);
+	}
+
+	public Node cloneGraphBFS(Node node) {
+		if (node == null) {
+			return null;
+		}
+
+		Map<Node, Node> cloneMap = new HashMap<>();
+		cloneMap.put(node, new Node(node.val, new ArrayList<>()));
+
+		Queue<Node> queue = new LinkedList<>();
+		queue.add(node);
+
+		while (!queue.isEmpty()) {
+			Node currNode = queue.poll();
+
+			for (Node neighbour : currNode.neighbors) {
+				if (cloneMap.get(neighbour) == null) {
+					cloneMap.put(neighbour,
+							new Node(neighbour.val, new ArrayList<>()));
+					queue.add(neighbour);// THis should noe be outside the if the condition, otherwise
+					                      // infinite loop happens because the queue is always adding nodes
+				}
+
+				cloneMap.get(currNode).neighbors.add(cloneMap.get(neighbour));
+			}
+		}
+		return cloneMap.get(node);
 	}
 }
 
