9d59a7ac87571ba7a252aca0ff1b872b5247a025
Abhra Kar <abhra.kar@gmail.com>
2025-03-24 11:53:52 +0530

    Update L53_MaximumSubarray.java

--- Top150/kadane/L53_MaximumSubarray.java
+++ Top150/kadane/L53_MaximumSubarray.java
@@ -1,14 +1,17 @@
 package kadane_algo;
 
+import java.util.Arrays;
+
 public class L53_MaximumSubarray {
 	public static void main(String[] args) {
-		int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+		int[] nums = {3, -4, 5, 4, -1, 7, -8};
 		System.out.println("Max Sum = " + maxSumArray(nums));
+		Arrays.stream(maxSumSubArrayLength(nums))
+				.forEach(i -> System.out.print(i + " "));
 	}
 
 	public static int maxSumArray(int[] nums) {
 		int maxSum = Integer.MIN_VALUE, currSum = 0;
-
 		for (int i = 0; i < nums.length; i++) {
 			currSum += nums[i];
 			maxSum = Math.max(maxSum, currSum);
@@ -18,5 +21,27 @@ public class L53_MaximumSubarray {
 			}
 		}
 		return maxSum;
+	}
+
+	// Below example to get the max sum subarray
+	public static int[] maxSumSubArrayLength(int[] nums) {
+		int maxSum = Integer.MIN_VALUE, currSum = 0;
+		int start = 0, finalStart = 0;
+		int end = 0;
+
+		for (int i = 0; i < nums.length; i++) {
+			currSum += nums[i];
+			if (currSum > maxSum) {
+				maxSum = currSum;
+				end = i;
+				finalStart = start;
+			}
+
+			if (currSum < 0) {
+				currSum = 0;
+				start = i + 1;
+			}
+		}
+		return new int[]{finalStart, end};
 	}
 }
