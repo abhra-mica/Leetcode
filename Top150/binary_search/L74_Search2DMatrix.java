b60ba40e9a4b332aa5355726a2f97aa3100d47a5
Abhra Kar <abhra.kar@gmail.com>
2025-03-25 15:07:43 +0530

    Update L74_Search2DMatrix.java

--- Top150/binary_search/L74_Search2DMatrix.java
+++ Top150/binary_search/L74_Search2DMatrix.java
@@ -2,32 +2,22 @@ package binary_search;
 
 public class L74_Search2DMatrix {
 	public static void main(String[] args) {
-		int[][] matrix = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}};
-		int target = 11;
+		// int[][] matrix = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}};
+		// int target = 11;
+		int[][] matrix = {{1, 3}};
+		int target = 3;
 		boolean result = searchMatrix(matrix, target);
 		System.out.println("Result= " + result);
 	}
-
 	public static boolean searchMatrix(int[][] matrix, int target) {
-		int start = 0;
-		int end = matrix.length - 1;
-		int mid = -1;
-		while (start <= end) {
-			mid = (start + end) / 2;
-			if (target >= matrix[mid][0]
-					&& target <= matrix[mid][matrix[mid].length - 1]) {
-				break;
-			} else if (matrix[mid][0] > target) {
-				end = mid - 1;
-			} else {
-				start = mid + 1;
-			}
+		int row = rowSearch(matrix, target);
+
+		if (row == -1) {
+			return false;
 		}
-
-		start = 0;
-		end = matrix[mid].length - 1;
-		int row = mid;
-
+		int start = 0;
+		int end = matrix[row].length - 1;
+		int mid = 0;
 		while (start <= end) {
 			mid = (start + end) / 2;
 			if (matrix[row][mid] == target) {
@@ -39,5 +29,23 @@ public class L74_Search2DMatrix {
 			}
 		}
 		return false;
+	}
+
+	private static int rowSearch(int[][] matrix, int target) {
+		int start = 0;
+		int end = matrix.length - 1;
+		int mid = 0;
+		while (start <= end) {
+			mid = (start + end) / 2;
+			if (target >= matrix[mid][0]
+					&& target <= matrix[mid][matrix[mid].length - 1]) {
+				return mid;
+			} else if (target < matrix[mid][0]) {
+				end = mid - 1;
+			} else {
+				start = mid + 1;
+			}
+		}
+		return -1;
 	}
 }
