725a133a665f0593d96bfe84bb2390e8e39e0856
Abhra Kar <abhra.kar@gmail.com>
2025-04-02 21:24:21 +0530

    Update L70_ClimbingStairs.java

--- Top150/ondD_dp/L70_ClimbingStairs.java
+++ Top150/ondD_dp/L70_ClimbingStairs.java
@@ -1,14 +1,53 @@
 package oneD_dp;
 
 public class L70_ClimbingStairs {
+	static int[] t = new int[46];
+
 	public static void main(String[] args) {
-		int result = climbStairs(4);
+		int result = climbStairs(1);
 		System.out.println(result);
 	}
 
-	public static int climbStairs(int n) {
-		if (n == 1) {
+	// 1.Using simple recurssion
+	public static int climbStairRecursion(int n) {
+		if (n < 0) {
+			return 0;
+		}
+
+		if (n == 0) {
 			return 1;
+		}
+
+		int oneStep = climbStairRecursion(n - 1);
+		int twoStep = climbStairRecursion(n - 2);
+		return oneStep + twoStep;
+	}
+
+	// 2.Recursion using memoization
+	public static int climbStairMemoization(int n) {
+		if (n < 0) {
+			return 0;
+		}
+		/*
+		 * We can initialize the array with -1 and check instead we way because
+		 * when n==0 the second if condition will handle that
+		 */
+		if (t[n] != 0) {
+			return t[n];
+		}
+		if (n == 0) {
+			return 1;
+		}
+
+		int oneStep = climbStairRecursion(n - 1);
+		int twoStep = climbStairRecursion(n - 2);
+		return t[n] = oneStep + twoStep;
+	}
+
+	// 3. Memoization bottom-up approach
+	public static int climbStairs(int n) {
+		if (n == 1 || n == 2 || n == 3) {
+			return n;
 		}
 
 		int[] dp = new int[n + 1];
