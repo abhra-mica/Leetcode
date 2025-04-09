fccafb07fdd5aad1ef1abd098d7da69d9255c1c7
Abhra Kar <abhra.kar@gmail.com>
2025-04-09 18:19:39 +0530

    Update L88_MergeSortedAarray.java

--- Top150/Array_String/L88_MergeSortedAarray.java
+++ Top150/Array_String/L88_MergeSortedAarray.java
@@ -1,3 +1,4 @@
+package array_string;
 
 public class L88_MergeSortedAarray {
 	public static void main(String[] args) {
@@ -5,40 +6,28 @@ public class L88_MergeSortedAarray {
 		 * int[] nums1 = {1, 2, 3, 0, 0, 0}; int m = 3; int[] nums2 = {2, 5, 6};
 		 * int n = 3;
 		 */
-
 		int[] nums1 = {2, 0};
 		int m = 1;
 		int[] nums2 = {1};
 		int n = 1;
 
 		merge(nums1, m, nums2, n);
-
 		for (int i : nums1) {
 			System.out.print(i + " ");
 		}
-
 	}
 
 	public static void merge(int[] nums1, int m, int[] nums2, int n) {
-		int firstPtr = m - 1;
-		int secondPtr = n - 1;
-		int k = m + n - 1;
-		while (secondPtr >= 0 && firstPtr >= 0) {
-			if (nums1[firstPtr] > nums2[secondPtr]) {
-				nums1[k] = nums1[firstPtr];
-				firstPtr--;
-			} else {
-				nums1[k] = nums2[secondPtr];
-				secondPtr--;
-			}
-			k--;
+		int k = nums1.length - 1;
+		m--;
+		n--;
+
+		while (m >= 0 && n >= 0) {
+			nums1[k--] = nums1[m] > nums2[n] ? nums1[m--] : nums2[n--];
 		}
 
-		// No need to check for firstPtr as we are merging in nums1 only, if
-		// secondPtr already traversed then the rest of the nums1 is already
-		// sorted
-		while (secondPtr >= 0) {
-			nums1[k--] = nums2[secondPtr--];
+		while (n >= 0) {
+			nums1[k--] = nums2[n--];
 		}
 	}
 }
