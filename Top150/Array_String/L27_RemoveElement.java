f9fbd22e938504d37b8a622d8406c9d082864d06
Abhra Kar <abhra.kar@gmail.com>
2025-10-27 08:18:51 +0530

    Update L27_RemoveElement.java

--- Top150/Array_String/L27_RemoveElement.java
+++ Top150/Array_String/L27_RemoveElement.java
@@ -1,41 +1,24 @@
 public class L27_RemoveElement {
-	public static void main(String[] args) {
-		// int[] nums = {0, 1, 2, 2, 3, 0, 4, 2};
-		// int val = 2;
-		int[] nums = {3};
-		int val = 3;
-		System.out.println(removeElement(nums, val));
-	}
-	
-	public static int removeElement(int[] nums, int val) {
-		int index = 0;
-		for (int i = 0; i < nums.length; i++) {
-			if (nums[i] != val) {
-				nums[index] = nums[i];
-				index++;
-			}
-		}
-		return index;
-	}
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 2, 2, 3, 0, 4, 2};
+        int val = 2;
+        int result = removeElement(nums, val);
+        System.out.println("No. of unique elements = " + result);
+    }
 
-	public static int removeElement(int[] nums, int val) {
-		int start = 0;
-		int end = nums.length - 1;
+    public static int removeElement(int[] nums, int val) {
+        int start = 0;
+        int end = nums.length - 1;
 
-		if (end < 0) { // If array is empty
-			return 0;
-		}
-		// The while loop will exist when start == end
-		while (start < end) {
-			if (nums[start] == val) {
-				nums[start] = nums[end--];
-			} else {
-				start++;
-			}
-		}
-		// if nums[start] == val then simply return start because 0 indexed
-		// array , if not then current nums[start] element also need to
-		// consider, so start + 1
-		return nums[start] == val ? start : start + 1;
-	}
+        while (start <= end) {
+            if (nums[start] == val) {
+                int temp = nums[start];
+                nums[start] = nums[end];
+                nums[end--] = temp;
+            } else {
+                start++;
+            }
+        }
+        return start;
+    }
 }
